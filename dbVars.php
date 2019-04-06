32489482ce9cdb7a3c4263d564e455140fc43f20
florianmac <nairolf3>
2019-04-06 13:33:17 +0200

    klaar
    klaar

---
+++ dbVars.php
@@ -0,0 +1,17 @@
+<?php
+// $servername="localhost";
+// $uid="root";
+// $pwd="";
+// $database="test";
+
+$servername = "127.0.0.1";
+
+
+$uid = "c4194test2";
+
+
+$pwd = "chfoddsw";
+
+
+$database = "c4194test2";
+?>
